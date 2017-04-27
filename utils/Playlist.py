480cfb41a933d4f77279439938a0bc8feadd1fb1
johnnyxh <johnnyxh@gmail.com>
2017-04-27 00:51:33 -0400

    Playlists not ending properly

--- utils/Playlist.py
+++ utils/Playlist.py
@@ -9,7 +9,6 @@ class Playlist:
 		self.songs = asyncio.Queue()
 		self.play_next_song = asyncio.Event()
 		self.current_song = None
-		self.audio_loop = self.bot.loop.create_task(self.play_next())
 
 	def get_commands(self):
 		commands = [
@@ -69,12 +68,16 @@ class Playlist:
 	async def play_next(self):
 		while True:
 			self.play_next_song.clear()
-			self.current_song = self.songs.get_nowait()
-			before_options = '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2'
-			self.bot.player = await self.bot.voice.create_ytdl_player(self.current_song.song, before_options=before_options, after=self.finished)
-			self.bot.player.volume = 0.45
-			self.bot.player.start()
-			await self.play_next_song.wait()
+			try:
+				self.current_song = self.songs.get_nowait()
+				before_options = '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2'
+				self.bot.player = await self.bot.voice.create_ytdl_player(self.current_song.song, before_options=before_options, after=self.finished)
+				self.bot.player.volume = 0.45
+				self.bot.player.start()
+				await self.play_next_song.wait()
+			except:
+				self.current_song = None
+				return
 
 	def finished(self):
 		self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
