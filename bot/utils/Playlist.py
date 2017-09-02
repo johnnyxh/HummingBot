e86140bdc6f98f8c72f4cfa5683ef6ae5500be1d
johnnyxh <johnnyxh@gmail.com>
2017-09-02 03:03:44 -0400

    feat(Playlist): Add support for downloading youtube playlists

--- bot/utils/Playlist.py
+++ bot/utils/Playlist.py
@@ -42,6 +42,11 @@ class Playlist:
 				'use': '?playlist skip'
 			},
 			{
+				'name': 'shuffle',
+				'description': 'Shuffle the order of the playlist',
+				'use': '?playlist shuffle'
+			},
+			{
 				'name': 'playing',
 				'description': 'Get information on the current songs in the playlist',
 				'use': '?playlist playing'
@@ -53,59 +58,62 @@ class Playlist:
 		try:
 			await self.bot.join_channel(message)
 
+			video_url = message.content.split()[2]
+
 			# Extract video information, possibly better in the SongEntry class
 			#TODO: May need to figure out how to use run_in_executor within SongEntry
 			opts = {
 				'format': 'webm[abr>0]/bestaudio/best',
 				'prefer_ffmpeg': True,
-				'noplaylist': True,
 				'verbose': True
 			}
+
+			youtube_qparams = parse_qs(urlparse(video_url).query) 
+			if 'list' in youtube_qparams: 
+				await self.bot.send_message(message.channel, 'Adding a youtube playlist to the song queue. This will take some time')
+				await self.bot.add_reaction(message, '🔄')
+
 			with youtube_dl.YoutubeDL(opts) as ydl:
-				func = functools.partial(ydl.extract_info, message.content.split()[2], download=False)
+				func = functools.partial(ydl.extract_info, video_url, download=False)
 				info = await self.bot.loop.run_in_executor(None, func)
 				if 'entries' in info:
-					info = info['entries'][0]
+					for entry in info['entries']:
+						new_song = SongEntry(message, entry['url'], entry)
+						await self.songs.put(new_song)
+					await self.bot.add_reaction(message, '🐦')
+				else:
+					new_song = SongEntry(message, video_url, info)
+					await self.songs.put(new_song)
+					print('Added: ' + new_song.title)
 
-			new_song = SongEntry(message, message.content.split()[2], info)
-			await self.songs.put(new_song)
+
 			await self.bot.add_reaction(message, '🐦')
 
-			print('Added: ' + new_song.title)
-
 			if not self.bot.is_playing() and self.current_song is None:
-				await self.play_next()
+				await self._play_next()
 		except Exception as err:
 			print(err)
 
 	async def pause(self, message):
-		if message.author.voice_channel is not None:
+		if self._user_in_voice_command(message.author):
 			self.bot.player.pause()
-			await self.bot.add_reaction(message, '🐦')
-		else:
-			await self.bot.send_message(message.channel, 'You should get in a voice channel first')
 
 	async def skip(self, message):
-		if message.author.voice_channel is not None:
+		if self._user_in_voice_command(message.author):
 			self.bot.player.stop()
-			await self.bot.add_reaction(message, '🐦')
-		else:
-			await self.bot.send_message(message.channel, 'You should get in a voice channel first')
+
+	async def shuffle(self, message):
+		if self._user_in_voice_command(message.author):
+			await self.bot.send_message(message.channel, 'Maybe someday')
 
 	async def clear(self, message):
-		if message.author.voice_channel is not None:
+		if self._user_in_voice_command(message.author):
 			self.songs = asyncio.Queue()
 			self.bot.player.stop()
-			await self.bot.add_reaction(message, '🐦')
-		else:
-			await self.bot.send_message(message.channel, 'You should get in a voice channel first')
 
 	async def resume(self, message):
-		if message.author.voice_channel is not None:
+		if self._user_in_voice_command(message.author):
 			self.bot.player.resume()
-			await self.bot.add_reaction(message, '🐦')
-		else:
-			await self.bot.send_message(message.channel, 'You should get in a voice channel first')
 
 	async def playing(self, message):
 		song_list = list(self.songs._queue)
@@ -113,24 +121,9 @@ class Playlist:
 		if (len(song_list) - 2) > 0: await self.bot.send_message(message.channel, 'There are ' + str(len(song_list) - 2) + ' other songs in the queue')
 
 		for song in song_list[1::-1]:
-			await self.bot.send_message(message.channel, embed=self.songEmbed(song, 'Coming up'))
+			await self.bot.send_message(message.channel, embed=self._songEmbed(song, 'Coming up'))
 
-		await self.bot.send_message(message.channel, embed=self.songEmbed(self.current_song, 'Now Playing'))
-
-	async def play_next(self):
-		while True:
-			self.play_next_song.clear()
-			self.current_song = None
-			try:
-				self.current_song = self.songs.get_nowait()
-				before_options = '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2'
-				self.bot.player = self.bot.voice.create_ffmpeg_player(self.current_song.player_url, before_options=before_options, after=self.finished)
-				print('Playing: ' + self.current_song.title)
-				self.bot.player.volume = 0.45
-				self.bot.player.start()
-				await self.play_next_song.wait()
-			except:
-				return
+		await self.bot.send_message(message.channel, embed=self._songEmbed(self.current_song, 'Now Playing'))
 
 	async def on_voice_state_update(self, before, after):
 		if self.bot.voice is not None and len(self.bot.voice.channel.voice_members) <= 1:
@@ -138,10 +131,33 @@ class Playlist:
 			self.bot.player.stop()
 			await self.bot.voice.disconnect()
 
-	def finished(self):
+	async def _play_next(self):
+		while True:
+			self.play_next_song.clear()
+			self.current_song = None
+			try:
+				self.current_song = self.songs.get_nowait()
+				before_options = '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2'
+				self.bot.player = self.bot.voice.create_ffmpeg_player(self.current_song.player_url, before_options=before_options, after=self._finished)
+				print('Playing: ' + self.current_song.title)
+				self.bot.player.volume = 0.45
+				self.bot.player.start()
+				await self.play_next_song.wait()
+			except:
+				return
+
+	async def _user_in_voice_command(self, user):
+		if user.voice_channel is not None:
+			await self.bot.add_reaction(message, '🐦')
+			return True
+		else:
+			await self.bot.send_message(message.channel, 'You should get in a voice channel first')
+			return False
+
+	def _finished(self):
 		self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
 
-	def songEmbed(self, song, description):
+	def _songEmbed(self, song, description):
 		#TODO: Have thumbnail logic in SongEntry
 		song_embed = discord.Embed(title=song.uploader + ' - ' + song.title, description=description, colour=0xDEADBF)
 		youtube_qparams = parse_qs(urlparse(song.url).query)
